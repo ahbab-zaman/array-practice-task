8bec056262e906ef91b3bff011b2d9a1713293e2
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-08-31 01:29:03 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,44 @@
+// Task 1
+// let fruits = ['mango', 'jackfruit', 'litchi', 'guava', 'jam'];
+// console.log(fruits[2]);
+// fruits[2]= 'blueberry';
+// console.log(fruits);
+
+// Task 2
+// let touristDestination = ['cha-bagan', 'airport', 'jaflong'];
+// console.log(touristDestination.push('shada-pathor'))
+// console.log(touristDestination);
+// let newPlace = touristDestination.push('bisna-kandi', 'ratar-gul');
+// console.log(touristDestination);
+// console.log(touristDestination.pop());
+// console.log(touristDestination);
+
+// Task 3
+
+// let books = ['zero to one', 'atomic habits', 'paradoxical sazid'];
+// console.log(books.includes('javascript'));
+
+// if (books.includes('javascript')) {
+//    console.log('you do not hav to pay')    
+// } else{
+//     console.log('Sorry!! You have to pay');
+// }
+
+// Task 4
+// let arr = [1, 2, 4, 5];
+// let city = '[45, 20, 10]';
+// console.log(Array.isArray('[45, 20, 10]'))
+// console.log(city);
+
+// if (Array.isArray(arr)) {
+//     console.log('Yes it is an array');    
+// } else{
+//     console.log('No, it is not an array');
+// }
+
+// Task 5
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [10, 20, 30, 40, 50];
+// let newArr = arr1.concat(arr2);
+// console.log(newArr);
